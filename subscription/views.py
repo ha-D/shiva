63bae54d45b8c714ab5a05b7f0a1a4473a49fdfc
hadi <h.d.zolfaghari@gmail.com>
2014-09-02 21:44:21 +0430

    Initial commit with subscription app

---
+++ subscription/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts 	 import render
+from subscription.models import Subscriber
+from subscription.forms  import SubscribeForm
+
+def index(request):
+	if request.method == 'POST':
+		form = SubscribeForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return render(request, 'subscription/index.html', { 'success': True })
+		return render(request, 'subscription/index.html', { 'success': False })
+	else:
+		form = SubscribeForm()
+	return render(request, 'subscription/index.html', { 'form': form })
